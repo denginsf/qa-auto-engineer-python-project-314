d8633c8267bb6d77e2c2ecdcfa4e9d74e85f389e
Aleksei Dengin <wrk.dengin@gmail.com>
2026-05-14 18:13:15 +0300

    fix for ci checks

--- conftest.py
+++ conftest.py
@@ -24,6 +24,7 @@ def driver():
     opts.add_argument("--headless=new")
     opts.add_argument("--disable-gpu")
     opts.add_argument("--window-size=1920,1080")
+    opts.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=opts)
     yield driver
     driver.quit()
