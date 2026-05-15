adf1d40f2808231223e871a44de16d8f7baf8035
Aleksei Dengin <wrk.dengin@gmail.com>
2026-05-15 23:23:38 +0300

    add opts.add_argument("--headless=new")

--- conftest.py
+++ conftest.py
@@ -26,6 +26,7 @@ def driver():
     opts.add_argument("--no-sandbox")
     opts.add_argument("--disable-dev-shm-usage")
     opts.add_argument("--disable-gpu")
+    opts.add_argument("--headless=new")
     opts.add_argument("--window-size=1920,1080")
     opts.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=opts)
